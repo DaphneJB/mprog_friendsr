616a3b00e43d7012b53d919669e91c0c0302aeae
Daphne JB <d.v.jonkersboth@amc.uva.nl>
2019-02-17 17:52:47 +0100

    Created Friendsclass

---
+++ app/src/main/java/com/example/friendsr/Friend.java
@@ -0,0 +1,39 @@
+package com.example.friendsr;
+
+import java.io.Serializable;
+
+public class Friend implements Serializable {
+    private String name, bio;
+    private int drawableId;
+    private float rating;
+
+    public Friend(String theName, String theBio, int aDrawableId) {
+        name = theName;
+        bio = theBio;
+        drawableId = aDrawableId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public int getDrawableId() {
+        return drawableId;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
